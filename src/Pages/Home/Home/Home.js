2dc2232265214b2fbaa56046fc5b775e7038409a
FahadwebD <fahadchowdhury66779@gmail.com>
2022-02-06 20:55:35 +0600

    banners added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Banner></Banner>
+            <AppointmentBanner></AppointmentBanner>
+        </div>
+    );
+};
+
+export default Home;
