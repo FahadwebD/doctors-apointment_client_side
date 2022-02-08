b7ad7d7727991310432f9ccffb36d110dd10a981
FahadwebD <fahadchowdhury66779@gmail.com>
2022-02-08 19:09:48 +0600

    initializing Firebase for Authentication

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjchkRUsrngtmK3WKcbOcoyqxTUwXrZ0k",
+    authDomain: "doctor-portal-c3eea.firebaseapp.com",
+    projectId: "doctor-portal-c3eea",
+    storageBucket: "doctor-portal-c3eea.appspot.com",
+    messagingSenderId: "253767550540",
+    appId: "1:253767550540:web:ccc65ad52e6f849aa016e9"
+  };
+
+  export default firebaseConfig;
